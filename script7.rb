fef2e368ea6f127720a3d0077beb6eee3149debf
Gladstone Rose <gladstonerose@hotmail.com>
2025-01-29 21:37:45 -0500

    add files

---
+++ script7.rb
@@ -0,0 +1,4 @@
+
+# Write your code on line 2!
+favorite_language ||= "Ruby"
+puts favorite_language
---
+++ script8.rb
@@ -0,0 +1,15 @@
+def multiple_of_three(n)
+   n % 3 == 0 ? "True" : "False"
+end
+puts multiple_of_three(3)
+
+
+=begin
+The Zen of Ruby
+Implicit Return
+2 min
+We know that methods in Ruby can return values, and we ask a method to return a value when we want to use it in another part of our program. What if we don’t put a return statement in our method definition, though?
+
+For instance, if you don’t tell a JavaScript function exactly what to return, it’ll return undefined. For Python, the default return value is None. But for Ruby, it’s something different: Ruby’s methods will return the result of the last evaluated expression.
+
+=end
